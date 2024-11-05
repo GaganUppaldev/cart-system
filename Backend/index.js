dc9439e0145f2646bdac3f204df886b71a4be2f4
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-11-05 20:16:07 +0530

    Update index.js
    created login endpoint that will find existence of user inside database (mongo 4.4 is used here)

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ app.use(cors({
 }));
 
 app.get('/', (req, res) => {
-    res.send('one two three mic check check check');
+    res.send('one two three mic check check check 1');
 });
 
 app.post('/test', async (req, res) => {
@@ -29,16 +29,37 @@ app.post('/test', async (req, res) => {
     console.log('Received name:', name);
     console.log('Received password:', password);
 
-    const user = new User({ name, password });
-    console.log('Created user instance:', user);
+    const entry = new User({ name, password });
+    console.log('Created user instance:', entry);
 
     try {
-        await user.save();
+        await entry.save();
         console.log('User saved successfully');
-        res.status(201).json({ message: 'User created', user });
+        res.status(201).json({ message: 'User created', entry });
     } catch (error) {
         console.error('Error creating user:', error);
         res.status(500).json({ message: 'Error creating user' });
+    }
+});
+
+// User login endpoint
+app.post('/login', async (req, res) => {
+    const { name, password } = req.body;
+
+    try {
+        // to find the user in the database
+        const user = await User.findOne({ name, password });
+
+        if (user) {
+            // If user exists, respond with status 420
+            res.status(200).json({ message: 'User exists' });
+        } else {
+            
+            res.status(401).json({ message: 'User not found or incorrect credentials' });
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).json({ message: 'Error during login' });
     }
 });
 
