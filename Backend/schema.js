cb2bf60e9c36eebad00c5ccc68b07ff0aece22d7
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-11-19 23:59:55 +0530

    Update schema.js

--- Backend/schema.js
+++ Backend/schema.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose'); // Import mongoose
 
-// Define the user schema and specify the collection name as 'userdata'
+// Define the user schema
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     password: { type: String, required: true },
+    cart: [
+        {
+            productId: Number,
+            
+        },
+    ],
 }, { collection: 'userdata' }); // Explicitly set collection name
 
 // Create a model using the schema
